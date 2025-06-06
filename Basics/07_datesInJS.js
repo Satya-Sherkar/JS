22f5595bf1066dec49b97d7890f35b8d4024d62c
satyamsherkar <satyasherkar@gmail.com>
2025-06-06 13:15:11 +0530

    Array in JS

---
+++ Basics/07_datesInJS.js
@@ -0,0 +1,27 @@
+// Date
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toUTCString());
+
+
+// let myspecificDate = new Date(2001, 0, 13);
+// console.log(myspecificDate.toDateString());
+
+// let TimeStamp = Date.now();
+// console.log(TimeStamp);
+
+// console.log(Math.floor(Date.now()/1000));
+
+console.log(myDate.toLocaleString(`default`, {
+    weekday: "short"}));
+
+
+
+
---
+++ Basics/08_array.js
@@ -0,0 +1,14 @@
+// arrays
+
+let ListOfBoys = [ "viraj", "atharv", "rudra", "adhira"]
+
+console.log(ListOfBoys.length);
+ListOfBoys.push("virat");
+
+console.log(ListOfBoys[2]);
+
+ListOfBoys.pop();
+console.log(ListOfBoys);
+
+
+
