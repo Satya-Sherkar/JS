c8198acca4a51ae558bb5be93eb6d1ad319e6d1f
satyamsherkar <satyasherkar@gmail.com>
2025-06-07 10:38:59 +0530

    Array

--- Basics/08_array.js
+++ Basics/08_array.js
@@ -1,14 +1,30 @@
 // arrays
 
-let ListOfBoys = [ "viraj", "atharv", "rudra", "adhira"]
+// let ListOfBoys = [ "viraj", "atharv", "rudra", "adhira"]
 
-console.log(ListOfBoys.length);
-ListOfBoys.push("virat");
+// console.log(ListOfBoys.length);
+// ListOfBoys.push("virat");
 
-console.log(ListOfBoys[2]);
+// console.log(ListOfBoys[2]);
 
-ListOfBoys.pop();
-console.log(ListOfBoys);
+// ListOfBoys.pop();
+// console.log(ListOfBoys);
+
+let marvelHeros = ["IronMan", "Thor", "Hulk"]
+
+let DCHeros = ["Batman", "Superman", "Flash"]
+
+//let multiverse = marvelHeros.concat(DCHeros)  // .concat combines two arrays and returns an array
+//console.log(multiverse);
+
+const AllNewHeros = [...DCHeros, ...marvelHeros]  //spreads all elements and combines
+//console.log(AllNewHeros);
+
+const crazyArray = [1, 2, 3, [12, 56, 135], 4, 5, [2, 8, [5,3]]]
+const goodArray = crazyArray.flat(Infinity)
+
+console.log(goodArray);
+
 
 
 
