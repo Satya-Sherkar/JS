f21cd84e04fbf0b8ed4df5cdab0efc3729a7ef80
satyamsherkar <satyasherkar@gmail.com>
2025-06-11 10:19:18 +0530

    ForEach

---
+++ Loops/five.js
@@ -0,0 +1,40 @@
+// forEach
+
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+coding.forEach( function funName(value) {
+    console.log(value);
+} );
+
+coding.forEach( (item) => {
+    console.log(item);
+} );
+
+function printMe(item) {
+    console.log(item);
+}
+
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+} )
+
+const myCoding = [
+  {
+    languageName: "JavaScript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "Java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
+
+myCoding.forEach((item) => {
+  console.log(item.languageFileName);
+});
